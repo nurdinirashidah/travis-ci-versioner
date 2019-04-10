92a5b04c6ec94111cc7241495b4eef75a4942989
Bhavik Kumar <bhavikk85@gmail.com>
2019-04-11 11:36:05 +1200

    Apply version to all projects

--- src/main/java/com/bnc/gradle/VersionPlugin.java
+++ src/main/java/com/bnc/gradle/VersionPlugin.java
@@ -1,6 +1,5 @@
 package com.bnc.gradle;
 
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -8,15 +7,8 @@ public class VersionPlugin implements Plugin<Project> {
 
   @Override
   public void apply(Project project) {
-    project.getExtensions().create("travisVersioner", VersionPluginExtension.class);
-    project.getTasks().create("version", VersionTask.class);
-
-    project.afterEvaluate(new Action<Project>() {
-      @Override
-      public void execute(Project evaluatedProject) {
-        VersionPluginExtension versionConfiguration = project.getExtensions().findByType(VersionPluginExtension.class);
-        project.setVersion(versionConfiguration.toString());
-      }
-    });
+    project.getTasks().create("showVersion", VersionTask.class);
+    VersionPluginExtension versionConfiguration = project.getExtensions().create("travisVersioner", VersionPluginExtension.class);
+    project.getAllprojects().forEach(subProject -> project.setVersion(versionConfiguration.toString()));
   }
 }
